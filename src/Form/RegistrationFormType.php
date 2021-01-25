3149c161e04398856ec9eb913b51b2155de08f0c
Yassine BELKEDAH <Yassine@macbook-pro-de-maissam.home>
2021-01-25 09:31:39 +0100

    Ajout btn connexion inscription

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,6 +6,10 @@ use App\Entity\Membre;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,31 +21,65 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('pseudo')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
+        ->add('pseudo')
+        
+        ->add('password', RepeatedType::class, [
+            'type' => PasswordType::class,
+            'invalid_message' => 'Le mot de passe saisi ne correspond pas',
+            'label' => 'Votre mot de passe',
+            'required' => true,
+            'first_options' => [
+                'label' => 'Mot de passe',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre mot de passe'
+                ]],
+            'second_options' => [
+                'label' => 'Confirmation du mot de passe',
+                'attr' => [
+                    'placeholder' => 'Confirmez votre mot de passe'
+                ]],
+        ])
+
+        ->add('prenom', TextType::class, [
+            "label" => "Prénom",
+        
+        ])
+        ->add('nom', TextType::class, [
+        
+        ])
+
+        ->add('email', EmailType::class, [
+
+        ])
+        
+        ->add('adresse', TextType::class, [
+        
             ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
-            ])
+
+        ->add('cp', TextType::class, [
+            'label' => 'Code postal',
+        ])
+
+        ->add('ville', TextType::class, [
+            
+        ])
+
+        ->add('agreeTerms', CheckboxType::class, [
+            'mapped' => false,
+            'constraints' => [
+                new IsTrue([
+                    'message' => 'Vous devez accepter nos condition d\'utilisation',
+                ]),
+            ],
+            "label" => "C.G.U"
+        ])
+
+        ->add('inscrire', SubmitType::class,[
+            "label" => "S'inscrire",
+            "attr" => [
+                "class" => "btn amado-btn w-100"
+            ]
+        ])
         ;
     }
 
